725a2dce90adf728a43c41ba322406c8363ee68f
mcliff1 <matt@denvercliffs.com>
2018-02-13 19:42:59 +0000

    added git init stuff

--- loadlibs.R
+++ loadlibs.R
@@ -1,4 +1,6 @@
 # bootstrap
+system("git config --global user.name 'Matt Cliff'")
+system("git config --global user.email 'matt@denvercliffs.com'")
 
 install.packages("ggplot2")
 install.packages("DAAG")
